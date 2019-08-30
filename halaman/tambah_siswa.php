d3c2e5a5ecd4fc5684537b2de543efb6165a7be5
Asadullohil Ghalib Kubat <ghalib.developer@gmail.com>
2019-08-30 14:48:14 +0700

    Initial commit

---
+++ halaman/tambah_siswa.php
@@ -0,0 +1,12 @@
+<h1 class="h3 mb-4 text-gray-800">Tambah Siswa</h1>
+<form action="">
+<div class="form-group">
+        <label for="nis">NIS</label>
+        <input type="text" class="form-control form-control-user" id="nis" placeholder="Nomor Induk Siswa">
+    </div>
+    <div class="form-group">
+        <label for="nama">Nama Lengkap</label>
+        <input type="text" class="form-control form-control-user" id="nama" placeholder="Nama Lengkap">
+    </div>
+    <a href="login.html" class="btn btn-primary btn-user btn-block">Tambah Siswa</a>
+</form>
